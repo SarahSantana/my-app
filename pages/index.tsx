33590c5cbcb93fe94edc6eebab8a480624371174
Sarah Santana Pereira <sarah.spereira@einstein.br>
2022-08-24 16:19:35 -0300

    ädicionando página de erro e link na index para demais posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,17 @@ import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
+const allPost = [
+  {
+    title: 'SSG SSR',
+    id: 'ssg-ssr',
+  },
+  {
+    title: 'Pré rendering',
+    id: 'pre-rendering',
+  }
+]
+
 export default function Home() {
   return (
     <Layout home>
@@ -16,6 +27,17 @@ export default function Home() {
             Oi me chamo José da Silva{' '}
             <Link href="/posts/first-post">page</Link>.)
           </p>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPost.map(({ id, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+              </li>
+            ))}
+          </ul>
         </section>
       </>
     </Layout>
